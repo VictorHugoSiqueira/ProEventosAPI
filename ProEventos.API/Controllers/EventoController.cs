6a974f1a1dd9082a5a351fb631d4bed49fc522a2
Victor Hugo Fernandes <huggofernandes@hotmail.com>
2021-06-17 16:16:53 -0300

    Codigo Inicial

---
+++ ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Persistence;
+using ProEventos.Domain;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class EventoController : ControllerBase
+    {
+
+        private readonly ProEventosContext _context;
+
+        public EventoController(ProEventosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _context.Eventos;
+        }
+        
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(evento => evento.Id == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "exemplo de post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"exemplo Put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "exemplo delete";
+        }
+    }
+}
---
+++ ProEventos.API/Data/ProEventosContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.API.Persistence
+{
+    public class ProEventosContext : DbContext
+    {
+
+        public ProEventosContext(DbContextOptions<ProEventosContext> options) : base(options) { }
+        
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
